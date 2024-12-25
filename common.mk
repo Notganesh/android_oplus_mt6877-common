b46c57455b2e61455e1f88191e0542494f1dd4f9
Alone0316 <70204635+Alone0316@noreply.github.com>
2024-12-25 08:30:23 +0530

    ossi:imported minimal rootdir

--- common.mk
+++ common.mk
@@ -6,6 +6,25 @@
 
 COMMON_PATH := device/oplus/mt6877-common
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.connectivity.common.rc \
+    init.recovery.mt6877.rc \
+    init.connectivity.rc \
+    init.modem.rc \
+    init.oplus.rc \
+    init.mt6877.rc \
+    init.mt6877.usb.rc \
+    init.mt6877.power.rc \
+    init.project.rc \
+    init.sensor_2_0.rc \
+    init.target.rc \
+    init_conninfra.rc \
+    fstab.mt6877 \
+    fstab.mt6877.ramdisk \
+    ueventd.oplus.rc \
+    ueventd.mtk.rc
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
