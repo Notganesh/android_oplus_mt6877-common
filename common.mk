39da8a7c603ec003d64e6f31db7c2ad573e9beca
Notganesh <chimeports@gmail.com>
2024-12-25 12:04:32 +0530

    ossi:Device uses dynamic partition

--- common.mk
+++ common.mk
@@ -53,6 +53,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.oplus
 
+# Dynamic Partition
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_BUILD_SUPER_PARTITION := false
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0.vendor \
